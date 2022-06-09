3cd1241f77f9c5d51aabd3e9ef4f8f566e8217fc
jinwoo6531 <jintakk6531@gmail.com>
2022-06-09 16:34:11 +0900

    tdd 기초 study

---
+++ src/mock/check/check.js
@@ -0,0 +1,10 @@
+function check(predicate, onSuccess, onFail) {
+  if (predicate()) {
+    onSuccess('yes');
+  } else {
+    onFail('no');
+  }
+}
+
+
+module.exports = check;
---
+++ src/mock/product/test/product_service_no_di.test.js
@@ -0,0 +1,11 @@
+const ProductService = require('../product_service_no_di');
+const ProductClient = require('../product_client.js');
+
+
+describe('ProductService', () => {
+  let productService;
+
+  beforeEach(() => {
+    productService = new ProductService();
+  })
+})
